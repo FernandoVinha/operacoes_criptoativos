39705c583ee739dd1b3f25667a4ee34f7f139d01
Fernando H Vinha <41375095+FernandoVinha@users.noreply.github.com>
2024-06-25 13:05:50 -0300

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -13,6 +13,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('email', 'password')}),
         ('Informações Pessoais', {'fields': ('nome_corretora', 'cnpj', 'url', 'endereco', 'pais', 'tipo_ni', 'tipo_de_registro')}),
         ('Permissões', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
@@ -22,6 +23,7 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ('email', 'nome_corretora', 'cnpj')
     ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
     def registro(self, obj):
         return obj.get_registro_0000()
@@ -32,6 +34,6 @@ admin.site.register(CustomUser, CustomUserAdmin)
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     form = ClienteForm
-    list_display = ('corretora','nome', 'cpf_cnpj', 'tipo_ni', 'corretora', 'endereco', 'pais')
+    list_display = ('corretora', 'nome', 'cpf_cnpj', 'tipo_ni', 'corretora', 'endereco', 'pais')
     search_fields = ('nome', 'cpf_cnpj')
     list_filter = ('cpf_cnpj', 'corretora')
